6def6c02243cf3a5b6cd099748b0e621aa9cdc70
Elie CHARRA <elie.charra@kibatic.com>
2017-05-18 17:01:04 +0200

    Initial commit

---
+++ DataCollector/HACollector.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Kitpages\HABundle\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+class HACollector extends DataCollector
+{
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data = array(
+            'host' => gethostname(),
+            'headers' => $request->headers->all(),
+            'client_ips' => $request->getClientIps(),
+            'http_host' => $request->getHttpHost(),
+            'trusted_proxies' => $request->getTrustedProxies(),
+            'local_ip' => $request->server->get('SERVER_ADDR'),
+            'is_secure' => $request->isSecure()
+        );
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getName()
+    {
+        return 'kitpages_ha.ha_collector';
+    }
+}
---
+++ KitpagesHABundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kitpages\HABundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KitpagesHABundle extends Bundle
+{
+}
